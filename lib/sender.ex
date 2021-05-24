4167bc43c477bee1449d18484906a73f2af3ea14
imlachR <rodrimlach@gmail.com>
2021-05-24 17:21:48 -0300

    Initial Commit

---
+++ lib/sender.ex
@@ -0,0 +1,30 @@
+defmodule Sender do
+  @moduledoc """
+  Documentation for `Sender`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sender.hello()
+      :world
+
+  """
+  def send_email("konnichiwa@world.com" = email), do:
+    raise "Oops, couldn't send the email to #{email}"
+
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
+
+  def notify_all(emails) do
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
+    |> Enum.to_list()
+  end
+
+end
